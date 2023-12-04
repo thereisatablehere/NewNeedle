c0683a1ac81c22c7a974487aa2c1dcd70c2cf41b
CarpetFloor <dbartness001@csbsju.edu>
2023-12-04 01:41:32 -0600

    Add and remove buttons animations

--- sequenceInputs.js
+++ sequenceInputs.js
@@ -1,18 +1,86 @@
 let parent = document.getElementById("inputsSequences");
 
+let addClickable = true;
+let addRef = document.getElementById("addButton");
+let anim = {
+    frame: 0, 
+    // set during reset function (always called before animation)
+    frames: 10, 
+    // set during reset function (always called before animation)
+    offsetChange: -1, 
+    currentOffset: 0, 
+    reset: function(adding) {
+        this.offsetChange = addRef.clientHeight / this.frames;
+        this.currentOffset = 0;
+        this.frame = 0;
+    }, 
+    animate: function(adding) {
+        addClickable = false;
+        removeClickable = false;
+        this.reset(adding);
+        animation(adding);
+    }
+}
+
+let removeClickable = false;
+let removeRef = document.getElementById("removeButton");
+
 function removeSequence() {
-    if(parent.children.length > 2) {
-        parent.removeChild(parent.lastElementChild);
+    if(removeClickable) {
+        anim.animate(false);
+    }
+}
+
+function animation(adding) {
+    // if removing, remove element at start of animation: 
+    if(!(adding) && anim.frame == 0) {
+        if(parent.children.length > 2) {
+            parent.removeChild(parent.lastElementChild);
+        }
+        // don't play animation if not removing anything because only 2 sequence inputs
+        else {
+            anim.frame = anim.frames;
+        }
+    }
+    // normal animation
+    if(anim.frame < anim.frames) {
+        ++anim.frame;
+        anim.currentOffset += anim.offsetChange;
+
+        negative = adding ? 1 : -1;
+
+        addRef.style.transform = "translateY(" + (anim.currentOffset * negative) + "px)";
+        removeRef.style.transform = "translateY(" + (anim.currentOffset * negative) + "px)";
+
+        if(!(adding) && (anim.frame == 4)) {
+            anim.frame = anim.frames;
+        }
+
+        window.setTimeout(()=>{animation(adding)}, 1000/60);
+    }
+    // animation finished
+    else {
+        if(adding) {
+            let element = document.createElement("input");
+            parent.appendChild(element);    
+            // add input validation function to input
+            element.addEventListener("input", function(){
+                validateInputSequence(element, true)
+            });
+        }
+
+        removeRef.style.transform = "none";
+        addRef.style.transform = "none";
+
+        addClickable = true;
+        removeClickable = true;
     }
 }
 
 function addSequence() {
-    let element = document.createElement("input");
-    parent.appendChild(element);    
-    // add input validation function to input
-    element.addEventListener("input", function(){
-        validateInputSequence(element, true)
-    });
+    if(addClickable) {
+        anim.animate(true);
+    }
 }
 
 const acceptedCharsSequence = "ABCDEFGHIJKLMNOPQRSTUVWXYZ-";
